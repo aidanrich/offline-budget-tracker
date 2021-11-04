b6ee8620053bad34466602c6e2557ff0e1ffb095
aorich <rich.aidan@gmail.com>
2021-11-04 17:01:13 -0400

    work with tutor, removed uneeded code

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,12 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   //add more
+  "/db.js",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/styles.css"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -19,22 +25,6 @@ self.addEventListener("install", function(evt) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
 
 // fetch
 self.addEventListener("fetch", function(evt) {
